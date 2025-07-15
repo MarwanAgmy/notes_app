e4c067c4a057a43d05a1edc92999f86bf684ae32
marwan agmy <marwanagmy53@gmail.com>
2025-07-15 21:22:11 +0300

    add simple bloc observer

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/widgets/constants.dart';
+import 'package:notes_app/constants.dart';
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -4,17 +4,6 @@ import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'add_note_form.dart';
 
-// class AddNoteBottomSheet extends StatelessWidget {
-//   const AddNoteBottomSheet({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Padding(
-//       padding: EdgeInsets.symmetric(horizontal: 16),
-//       child: SingleChildScrollView(child: AddNoteForm()),
-//     );
-//   }
-// }
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
