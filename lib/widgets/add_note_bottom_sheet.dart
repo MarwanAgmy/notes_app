e3ea3747850c330164fd77c131283137b39bc1b3
marwan agmy <marwanagmy53@gmail.com>
2025-07-16 16:07:19 +0300

    fix issue for keyboard

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -28,13 +28,12 @@ class AddNoteBottomSheet extends StatelessWidget {
                 return AbsorbPointer(
                   absorbing: state is AddNoteLoading ? true : false,
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: SingleChildScrollView(
-                      padding: EdgeInsets.only(
-                        bottom: MediaQuery.of(context).viewInsets.bottom,
-                      ),
-                      child: const AddNoteForm(),
+                    padding: EdgeInsets.only(
+                      left: 16,
+                      right: 16,
+                      bottom: MediaQuery.of(context).viewInsets.bottom,
                     ),
+                    child: const SingleChildScrollView(child: AddNoteForm()),
                   ),
                 );
               },
