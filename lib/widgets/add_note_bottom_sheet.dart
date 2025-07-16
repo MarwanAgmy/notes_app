89dd2c77ee665d547d96e9d373a9eb4061be2a5c
marwan agmy <marwanagmy53@gmail.com>
2025-07-16 15:25:06 +0300

    change add note loading

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -26,13 +25,16 @@ class AddNoteBottomSheet extends StatelessWidget {
           builder: (context, state) {
             return Builder(
               builder: (context) {
-                return ModalProgressHUD(
-                  inAsyncCall: state is AddNoteLoading ? true : false,
-                  child: SingleChildScrollView(
-                    padding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                return AbsorbPointer(
+                  absorbing: state is AddNoteLoading ? true : false,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: SingleChildScrollView(
+                      padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom,
+                      ),
+                      child: const AddNoteForm(),
                     ),
-                    child: const AddNoteForm(),
                   ),
                 );
               },
