4183376163d2cbdf1cb07b091e82182d9133723e
marwan agmy <marwanagmy53@gmail.com>
2025-07-19 17:40:34 +0300

    display notes part one

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/constants.dart';
+import 'package:meta/meta.dart';
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
